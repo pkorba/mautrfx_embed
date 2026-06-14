f599e95a48c82c79cd2c7fc13907efbdbce43013
pkorba <32198274+pkorba@users.noreply.github.com>
2026-06-14 18:56:53 +0200

    Bump version to v2.1.1

--- mautrfx_embed/resources/utils.py
+++ mautrfx_embed/resources/utils.py
@@ -31,7 +31,7 @@ class Utilities:
         self.files = files
         self.config = self.bot.config
         self.headers = {
-            "User-Agent": "MautrFxEmbedBot/2.1.0"
+            "User-Agent": "MautrFxEmbedBot/2.1.1"
         }
         self.headers_fake = {
             "User-Agent": "WhatsApp/2"
